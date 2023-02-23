dd1d9bdf11fc6cb3493e99caf32807dba38e5f3c
Techguy1994 <maroun.elchayeb@gmail.com>
2023-02-23 11:51:52 +0100

    removed all .tflite

--- pyarmnn/Conversion/onnx_to_tensorflow.py
+++
@@ -1,8 +0,0 @@
-# source: https://github.com/onnx/onnx-tensorflow/blob/master/example/onnx_to_tf.py
-
-import onnx
-from onnx_tf.backend import prepare
-
-onnx_model = onnx.load("ImageClassifier.onnx")  # load onnx model
-tf_rep = prepare(onnx_model)  # prepare tf representation
-tf_rep.export_graph("tf_class")  # export the model
